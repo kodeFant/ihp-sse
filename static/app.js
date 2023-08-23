b8acde1622b5457f720c1b046930a30d60c54025
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2023-08-23 13:17:55 +0200

    Re-enable turbolinks

--- static/app.js
+++ static/app.js
@@ -14,6 +14,7 @@ function initializeEventSource() {
     return eventSource;
 }
 
+let eventSource = initializeEventSource();
 
 function closeEventSource() {
     console.log("Closing connection.");
@@ -21,8 +22,6 @@ function closeEventSource() {
         eventSource.close();
     }
 }
-
-let eventSource = initializeEventSource();
 
 document.addEventListener('turbolinks:load', function () {
     eventSource = initializeEventSource()
