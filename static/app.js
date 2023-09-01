20ed839c83dd9bee9c257acde2b6a63d4380a03b
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2023-09-01 15:38:45 +0200

    Fix js

--- static/app.js
+++ static/app.js
@@ -4,60 +4,60 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 
-function initializeEventSource() {
-    const eventSource = new EventSource("/StreamPostsEvents");
+// function initializeEventSource() {
+//     const eventSource = new EventSource("/StreamPostsEvents");
 
-    eventSource.onopen = function () {
-        console.log("Connection opened.");
-    };
-    eventSource.onerror = function (err) {
-        console.error("EventSource failed:", err);
-    };
-    eventSource.onmessage = function (event) {
-        console.log("Received message:", event.data);
-    };
+//     eventSource.onopen = function () {
+//         console.log("Connection opened.");
+//     };
+//     eventSource.onerror = function (err) {
+//         console.error("EventSource failed:", err);
+//     };
+//     eventSource.onmessage = function (event) {
+//         console.log("Received message:", event.data);
+//     };
 
-    eventSource.addEventListener('ping', function (e) {
-        console.log('ping', e.data);
-    }, false);
+//     eventSource.addEventListener('ping', function (e) {
+//         console.log('ping', e.data);
+//     }, false);
 
-    eventSource.addEventListener('posts_updated', function (e) {
-        const notificationSound = new Audio("/msn-sound.wav")
-        notificationSound.play();
-        console.log('posts_Updated', e);
-    }, false);
+//     eventSource.addEventListener('posts_updated', function (e) {
+//         const notificationSound = new Audio("/msn-sound.wav")
+//         notificationSound.play();
+//         console.log('posts_updated', e);
+//     }, false);
 
-    return eventSource;
-}
+//     return eventSource;
+// }
 
-let eventSource = initializeEventSource();
+// let eventSource = initializeEventSource();
 
-function closeEventSource() {
-    console.log("Closing connection.");
-    if (eventSource != null) {
-        eventSource.close();
-    }
-}
+// function closeEventSource() {
+//     console.log("Closing connection.");
+//     if (eventSource != null) {
+//         eventSource.close();
+//     }
+// }
 
-document.addEventListener('turbolinks:load', function () {
-    eventSource = initializeEventSource()
-});
+// document.addEventListener('turbolinks:load', function () {
+//     eventSource = initializeEventSource()
+// });
 
-document.addEventListener('DOMContentLoaded', function () {
-    eventSource = initializeEventSource();
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     eventSource = initializeEventSource();
+// });
 
-document.addEventListener('turbolinks:before-cache', function () {
-    closeEventSource();
-});
+// document.addEventListener('turbolinks:before-cache', function () {
+//     closeEventSource();
+// });
 
-document.addEventListener('turbolinks:before-visit', function () {
-    closeEventSource();
-});
+// document.addEventListener('turbolinks:before-visit', function () {
+//     closeEventSource();
+// });
 
 
-document.addEventListener('beforeunload', function () {
-    closeEventSource();
-});
+// document.addEventListener('beforeunload', function () {
+//     closeEventSource();
+// });
 
 
